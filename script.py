8981141b6c2ec9ed8c5a72eb8f08354ac27020e8
twisteroid ambassador <twisteroidambassador@users.noreply.github.com>
2019-07-26 01:45:42 +0800

    Improve example script.

--- script.py
+++ script.py
@@ -1,46 +1,42 @@
 #! /usr/bin/env python3
+"""Example script for UDPack. Requires Python 3.7 or above, but should be
+easily adaptable down to Python 3.4.
 
+Creates a pair of packer / unpacker that emulated OpenVPN's "XOR patch".
+"""
 import asyncio
 import functools
 import logging
 
 import udpack
 
-logging.basicConfig(level=logging.DEBUG)
-logging.getLogger().setLevel(logging.DEBUG)
 
-def main():
-    
-    loop = asyncio.get_event_loop()
-    
-    pipeline = [udpack.NoOpPacker, udpack.ReverseOnePlusPacker, udpack.ReverseOnePlusPacker]
-    pack_pipeline = [udpack.XorPtrPosPacker, 
-                     udpack.ReverseOnePlusPacker, 
-                     udpack.XorPtrPosPacker, 
-                     functools.partial(udpack.XorMaskPacker, b'hello world!')]
-    unpack_pipeline = [functools.partial(udpack.XorMaskPacker, b'hello world!'),
-                       udpack.XorPtrPosPacker, 
-                       udpack.ReverseOnePlusPacker, 
-                       udpack.XorPtrPosPacker]
-    transport, protocol = loop.run_until_complete(udpack.create_udpack(
-            loop, 
-            functools.partial(udpack.PipelineManager, 
-                              pack_pipeline,
-                              False),
-            ('127.0.0.1', 7000),
-            ('127.0.0.1', 7050),
-            10, 20))
-    transport, protocol = loop.run_until_complete(udpack.create_udpack(
-            loop, 
-            functools.partial(udpack.PipelineManager, 
-                              pack_pipeline,
-                              True),
-            ('127.0.0.1', 7050),
-            ('127.0.0.1', 7100),
-            10, 20))
+async def run_packers():
+    loop = asyncio.get_running_loop()
+    # Create packer pipeline, emulating the XOR patch
+    pipeline = [udpack.XorPtrPosPacker,
+                udpack.ReverseOnePlusPacker,
+                udpack.XorPtrPosPacker,
+                functools.partial(udpack.XorMaskPacker, b'hello world!')]
+    # Create client-side packer
+    await udpack.create_udpack(
+        loop,
+        functools.partial(udpack.PipelineManager, pipeline, False),
+        ('127.0.0.1', 7000),
+        ('127.0.0.1', 7050),
+        10, 20)
+    # Create server-side unpacker
+    await udpack.create_udpack(
+        loop,
+        functools.partial(udpack.PipelineManager, pipeline, True),
+        ('127.0.0.1', 7050),
+        ('127.0.0.1', 7100),
+        10, 20)
+    # Wait forever
     while True:
-        loop.run_until_complete(asyncio.sleep(1))
+        await asyncio.sleep(1)
 
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.DEBUG)
+    asyncio.run(run_packers())
